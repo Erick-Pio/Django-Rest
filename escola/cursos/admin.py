8e5549fd552d57ac9bb15891aff4194daa2daf13
unknown <erick.pio@sptech.school>
2025-07-17 23:56:35 -0300

    Subindo aula sobre Django, Aula 1 até inicio de uso do Model Serializer

---
+++ escola/cursos/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import Curso, Avalicao
+
+
+
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'url', 'criacao', 'atualizacao', 'ativo')
+
+@admin.register(Avalicao)
+class AvaliacaoAdmin(admin.ModelAdmin):
+    list_display = ('curso', 'nome', 'email', 'avaliacao', 'criacao', 'atualizacao', 'ativo')
+
